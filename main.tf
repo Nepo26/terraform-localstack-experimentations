2bbb82e3ffc05aeb755bf544aa53d1283b111b7d
henrique <henrique.nepomuceno@musicplayce.com>
2020-10-16 12:33:52 -0300

    feat: input and output testing

--- main.tf
+++ main.tf
@@ -1,41 +1,70 @@
 provider "aws" {
-    access_key                  = "mock_access_key"
-    region                      = "us-east-2"
-    s3_force_path_style         = true
-    secret_key                  = "mock_secret_key"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    endpoints {
-        apigateway     = "http://localhost:4566"
-        cloudformation = "http://localhost:4566"
-        cloudwatch     = "http://localhost:4566"
-        dynamodb       = "http://localhost:4566"
-        es             = "http://localhost:4566"
-        ec2            = "http://localhost:4566"
-        firehose       = "http://localhost:4566"
-        iam            = "http://localhost:4566"
-        kinesis        = "http://localhost:4566"
-        lambda         = "http://localhost:4566"
-        route53        = "http://localhost:4566"
-        redshift       = "http://localhost:4566"
-        s3             = "http://localhost:4566"
-        secretsmanager = "http://localhost:4566"
-        ses            = "http://localhost:4566"
-        sns            = "http://localhost:4566"
-        sqs            = "http://localhost:4566"
-        ssm            = "http://localhost:4566"
-        stepfunctions  = "http://localhost:4566"
-        sts            = "http://localhost:4566"
+  access_key                  = "mock_access_key"
+  region                      = "us-east-2"
+  s3_force_path_style         = true
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
+
+variable "server_port" {
+  description = "The port which the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-bob-webserver-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "bob" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
-    Name = "Bob"
+    Name = "terraform-bob-webserver"
   }
+}
 
+output "security_group_id" {
+  value = aws_security_group.instance.id
+  description = "The id of the security group associated to bob"
 }
