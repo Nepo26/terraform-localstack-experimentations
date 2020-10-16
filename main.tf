d9970550a83975d9590405de42df02ac3cb8447f
henrique <henrique.nepomuceno@musicplayce.com>
2020-10-16 16:39:47 -0300

    feat: elb trying

--- main.tf
+++ main.tf
@@ -36,6 +36,16 @@ variable "server_port" {
   default     = 8080
 }
 
+# Setting datas to be able to create the auto scaler
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Security group of each instance created on the auto scaler
 resource "aws_security_group" "instance" {
   name = "terraform-bob-webserver-instance"
 
@@ -47,10 +57,11 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "bob" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+# Launch configurations to each instance created on the autoscaling group
+resource "aws_launch_configuration" "bob" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
 
   user_data = <<-EOF
@@ -59,12 +70,120 @@ resource "aws_instance" "bob" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  tags = {
-    Name = "terraform-bob-webserver"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-output "security_group_id" {
-  value = aws_security_group.instance.id
-  description = "The id of the security group associated to bob"
+# The actual autoscaling group
+resource "aws_autoscaling_group" "bob" {
+  launch_configuration = aws_launch_configuration.bob.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+
+# Configuring load balancer
+# ---------------------------------------------------------------------
+
+resource "aws_lb" "example" {
+  # Not yet sure why it is asg instead of lb
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# Setting up a security group because by default, all AWS resources, including ALBs, don't allow any incoming or outcoming traffic
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = 200
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+
+}
+
+
+# ---------------------------------------------------------------------
+
+
+
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
